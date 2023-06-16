5a68b31f9ae3e9098b402eeb2cce0a672b43ac7f
jhonvss1 <134210144+jhonvss1@users.noreply.github.com>
2023-06-15 21:50:03 -0300

    ola mundo

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -37,3 +37,49 @@ ReturnSucessDTO returnSucess = new ReturnSucessDTO()
 }; 
 
 Console.WriteLine ($"Message {returnSucess.Message}\n Name : {returnSucess.Name}\n Token {returnSucess.Token}");
+
+
+Console.WriteLine("------------ CALCULADORA DE CONSOLE --------------------");
+
+OperacoesAritmeticas operacoes = new OperacoesAritmeticas();
+
+Console.WriteLine("Informe seu nome: ");
+string Name = Console.ReadLine();
+
+// Receber números para a operação
+Console.WriteLine("Digite um número: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+// Escolher as operações realizavéis 
+Console.WriteLine("Qual operação deseja realizar? [+,-,*,/]: ");
+string operador = Console.ReadLine();
+
+
+Console.WriteLine("Digite outro número: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+// Realizar validação e operação
+if (operador == "+")
+{
+    int soma = number1 + number2;
+    Console.WriteLine("O resultado da soma é igual a " + soma);
+    return;
+}
+else if (operador == "-")
+{
+    double subtracao = number1 - number2;
+    Console.WriteLine("O resultado da Divisão é igual a : " + subtracao);
+    return;
+}
+else if (operador == "*")
+{
+    double multipllicacao = number1 * number2;
+    Console.WriteLine("A multiplicação dos dois valores informados é : " + multipllicacao);
+    return;
+}
+else if (operador == "/")
+{
+    double divisao = number1 / number2;
+    Console.WriteLine("A divisão entre os dois valores é : " + divisao);
+    return;
+}
